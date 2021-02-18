f438a61e5d748bead60db5a57b15ba0dfbae239e
abhis018 <76279317+abhis018@users.noreply.github.com>
2021-02-18 22:24:49 +0530

    upadate the image from backend data
    Signed-off-by: abhis018 <76279317+abhis018@users.noreply.github.com>

--- src/Backend/db.js
+++ src/Backend/db.js
@@ -1,105 +1,105 @@
 let product = [
     {
         "id":1,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/laptop.jpg",
+        "picture":"https://i.ibb.co/hBrZRgH/laptop.jpg",
         "price":62000,
         "rating": 3,
         "description":"A typical computer system consists of a computer case,a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":2,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/keyboard.png",
+        "picture":"https://i.ibb.co/cDHP3HK/keyboard.png",
         "price":2630,
         "rating": 4,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":3,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/mouse.jpg",
+        "picture":"https://i.ibb.co/LNx8Mpw/mouse.jpg",
         "price":750,
         "rating": 1,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":4,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/charger.jpg",
+        "picture":"https://i.ibb.co/XJwvsgB/charger.jpg",
         "price":3500,
         "rating": 3,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":5,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/batteries.jpg",
+        "picture":"https://i.ibb.co/PrZD3qT/batteries.jpg",
         "price":4000,
         "rating": 2,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":6,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/printer.jpg",
+        "picture":"https://i.ibb.co/BB9fgRF/printer.jpg",
         "price":15000,
         "rating": 4,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":7,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/headphones.jpg",
+        "picture":"https://i.ibb.co/D8tt3LZ/headphones.jpg",
         "price":4800,
         "rating": 4,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":8,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/speakers.jpg",
+        "picture":"https://i.ibb.co/VQxb8dT/speakers.jpg",
         "price":3000,
         "rating": 2,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":9,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/monitor.jpg",
+        "picture":"https://i.ibb.co/jRZ4CD7/monitor.jpg",
         "price":10000,
         "rating": 5,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":10,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/cpu.png",
+        "picture":"https://i.ibb.co/0hZ2mtn/cpu.png",
         "price":18000,
         "rating": 5,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":11,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/cooling-pad.jpg",
+        "picture":"https://i.ibb.co/wSq0MVP/cooling-pad.jpg",
         "price":1000,
         "rating": 3,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":12,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/pendrive.png",
+        "picture":"https://i.ibb.co/4TTpstT/pendrive.jpg",
         "price":1200,
         "rating": 2,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":13,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/web-cam.jpg",
+        "picture":"https://i.ibb.co/b7cLFzQ/web-cam.jpg",
         "price":2460,
         "rating": 1,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":14,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/hard-disk.jpg",
+        "picture":"https://i.ibb.co/ncNrRHF/External-hard-disk-with-cable-isolated-on-white-background-Small-modern-hard-drive-PC-security.jpg",
         "price":6000,
         "rating": 5,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
     },
     {
         "id":15,
-        "picture":"C:/Users/abhis/OneDrive/Desktop/selling-app/src/components/Body/game-pad.jpg",
+        "picture":"https://i.ibb.co/qgbGtt1/game-pad.jpg",
         "price":1700,
         "rating": 4,
         "description":"A typical computer system consists of a computer case a power supply unit, a motherboard, a central processing unit (CPU), main memory, and a hard disk drive.Input devices include a keyboard, mouse, microphone, video camera, and image scanner.Output devices include a monitor, speakers, and a printer."
